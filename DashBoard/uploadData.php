cc4c32f50295b641867711166ed38339d3ff8f1c
Junyang HE <nathanhejunyang@gmail.com>
2017-10-17 17:03:24 +0200

    updated unplad data file

--- DashBoard/uploadData.php
+++ DashBoard/uploadData.php
@@ -5,19 +5,21 @@ $temp = ( isset ( $_GET["t"] ) ) ? trim ( $_GET["t"] ) : '';
 $hum = ( isset ( $_GET["h"] ) ) ? trim ( $_GET["h"] ) : '';
 $food = ( isset ( $_GET["f"] ) ) ? trim ( $_GET["f"] ) : '';
 $water = ( isset ( $_GET["w"] ) ) ? trim ( $_GET["w"] ) : '';
+$foodpct = ( isset ( $_GET["fp"] ) ) ? trim ( $_GET["fp"] ) : '';
+$waterpct = ( isset ( $_GET["wp"] ) ) ? trim ( $_GET["wp"] ) : '';
 
 require_once "../config.php";
 require_once (ROOT_DIR.'/includes/param.inc.php');
 
-if($dateandtime != '' && (($temp != '' && $hum != '') || ($food != '' && $water != ''))){
+if(($dateandtime != '' && $temp != '' && $hum != '') || ($food != '' && $water != '' && $foodpct != '' && $waterpct != '')){
     $conn = new mysqli($host, $user, $dbpw, $db);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
         echo "Connection failed: $conn";
     } else {
-        $dateandtime = $conn->real_escape_string(urldecode($dateandtime));
         if($temp != '' && $hum != '') {
+            $dateandtime = $conn->real_escape_string(urldecode($dateandtime));
             $temp =  $conn->real_escape_string(urldecode($temp));
             $hum =  $conn->real_escape_string(urldecode($hum));
 
@@ -25,8 +27,10 @@ if($dateandtime != '' && (($temp != '' && $hum != '') || ($food != '' && $water 
         } else {
             $food =  $conn->real_escape_string(urldecode($food));
             $water =  $conn->real_escape_string(urldecode($water));
+            $foodpct =  $conn->real_escape_string(urldecode($foodpct));
+            $waterpct =  $conn->real_escape_string(urldecode($waterpct));
 
-            $sql = "INSERT INTO foodwater VALUES('$dateandtime', '$food', '$water')";
+            $sql = "INSERT INTO foodwater(food, foodpct, water, waterpct) VALUES('$food', '$foodpct', '$water', '$waterpct')";
         }
 	//echo $sql;
         if($conn->query($sql) === true){
